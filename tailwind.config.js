5116d70530a1501522a083184155f068faefe0a5
amryoussef92 <amryusif@gmail.com>
2025-02-12 23:52:51 +0400

    movies-commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "js/main.js"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#ff2c1f",
+        bg: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
